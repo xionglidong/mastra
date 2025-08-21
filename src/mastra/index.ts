6bcb56b3e7531057730acd6e11ba9c6c7cf7860c
xionglidong <1105188507@qq.com>
2025-08-21 15:09:19 +0800

    优化

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,7 +1,7 @@
 
 import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
-import { LibSQLStore } from '@mastra/libsql';
+// import { LibSQLStore } from '@mastra/libsql';
 import { CloudflareDeployer } from "@mastra/deployer-cloudflare";
 import { weatherWorkflow } from './workflows/weather-workflow';
 import { weatherAgent } from './agents/weather-agent';
@@ -9,10 +9,10 @@ import { weatherAgent } from './agents/weather-agent';
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
   agents: { weatherAgent },
-  storage: new LibSQLStore({
-    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:",
-  }),
+  // storage: new LibSQLStore({
+  //   // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+  //   url: ":memory:",
+  // }),
   logger: new PinoLogger({
     name: 'Mastra',
     level: 'info',
