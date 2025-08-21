e1f9881295daea877c00b8fe705ccfceb93329e5
xionglidong <1105188507@qq.com>
2025-08-21 14:32:19 +0800

    优化

--- src/mastra/agents/weather-agent.ts
+++ src/mastra/agents/weather-agent.ts
@@ -1,7 +1,7 @@
 import { openai } from '@ai-sdk/openai';
 import { Agent } from '@mastra/core/agent';
 import { Memory } from '@mastra/memory';
-import { LibSQLStore } from '@mastra/libsql';
+// import { LibSQLStore } from '@mastra/libsql';
 import { weatherTool } from '../tools/weather-tool';
 
 export const weatherAgent = new Agent({
@@ -23,8 +23,8 @@ export const weatherAgent = new Agent({
   model: openai('gpt-4o-mini'),
   tools: { weatherTool },
   memory: new Memory({
-    storage: new LibSQLStore({
-      url: 'file:../mastra.db', // path is relative to the .mastra/output directory
-    }),
+    // storage: new LibSQLStore({
+    //   url: 'file:../mastra.db', // path is relative to the .mastra/output directory
+    // }),
   }),
 });
